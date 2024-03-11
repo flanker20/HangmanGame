ae6b5c1df646e49dc6985ceddb72489a293d282e
stadler <stadler@actoll.com>
2024-03-11 16:42:48 +0100

    - hangman visual - key buttons

--- app/src/main/java/com/payxpert/game/HangmanFragment.kt
+++ app/src/main/java/com/payxpert/game/HangmanFragment.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -15,6 +17,7 @@ import com.payxpert.game.data.dataStore
 import com.payxpert.game.databinding.HangmanFragmentBinding
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
+
 
 /**
  * Fragment where the game is played, contains the game logic.
@@ -32,6 +35,8 @@ class HangmanFragment : Fragment() {
     private val viewModel: HangmanViewModel by viewModels()
 
     private lateinit var settingsDataStore: SettingsDataStore
+    private val imgs = intArrayOf(R.drawable.pendu0,R.drawable.pendu1,R.drawable.pendu2,R.drawable.pendu3,R.drawable.pendu4,R.drawable.pendu5,R.drawable.pendu6,R.drawable.pendu7,R.drawable.pendu8,R.drawable.pendu9,R.drawable.pendu10)
+    private lateinit var btnKeys: List<Button>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,7 +59,18 @@ class HangmanFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
 
         // Setup a click listener for the Submit and Skip buttons.
-        binding.gameinput.btnSubmitChar.setOnClickListener { onSubmitChar() }
+        btnKeys = listOf(binding.gameinput.a,binding.gameinput.b,binding.gameinput.c,binding.gameinput.d,binding.gameinput.e,binding.gameinput.f,binding.gameinput.g,binding.gameinput.h,binding.gameinput.i
+            ,binding.gameinput.j,binding.gameinput.k,binding.gameinput.l,binding.gameinput.m,binding.gameinput.n,binding.gameinput.o,binding.gameinput.p,binding.gameinput.q,binding.gameinput.r
+            ,binding.gameinput.s,binding.gameinput.t,binding.gameinput.u,binding.gameinput.v,binding.gameinput.w,binding.gameinput.x,binding.gameinput.y,binding.gameinput.z
+        )
+        for(btn in btnKeys) {
+            Log.d(TAG, "init button ${btn.text}")
+            btn.setOnClickListener {
+                it as Button
+                onSubmitChar(it.text[0])
+                it.isEnabled = false
+            }
+        }
         binding.gamestats.btnReset.setOnClickListener{ onResetScore() }
 
         getScoresFromDatastore()
@@ -77,6 +93,7 @@ class HangmanFragment : Fragment() {
     */
     private fun startNewGame() {
         Log.d(TAG, "startNewGame")
+        for(btn in btnKeys) btn.isEnabled = true
         viewModel.getNextWord()
         setMaskedWord()
         displayStats()
@@ -90,26 +107,21 @@ class HangmanFragment : Fragment() {
     /*
     * Checks the char, and updates the word accordingly.
     */
-    private fun onSubmitChar() {
-        val charSubmitted = binding.gameinput.txtCharToSubmit.text.toString()
-        Log.d(TAG, "Char submitted = $charSubmitted")
-        if (charSubmitted.isNotEmpty()) {
-            val charToTest = charSubmitted[0]
+    private fun onSubmitChar(c: Char) {
+        Log.d(TAG, "Char submitted = $c")
 
-            viewModel.isCharInWord(charToTest)
-            displayStats() // games stats updates after the first character submitted
+        viewModel.isCharInWord(c)
+//            displayStats() // games stats updates after the first character submitted
 
-            setMaskedWord()
-            binding.gameinput.txtCharToSubmit.text.clear()
-            if (isComplete(viewModel.maskedWord.value)) {
-                Log.i(TAG,"Success")
-                viewModel.endGame(true)
-                displayStats()
-                showFinalScoreDialog(getString(R.string.you_scored, viewModel.score.value, viewModel.games.value))
-            } else if (0 == viewModel.tries.value) {
-                viewModel.endGame(false)
-                showFinalScoreDialog(getString(R.string.you_loose, viewModel.wordToFind.value))
-            }
+        setMaskedWord()
+        if (isComplete(viewModel.maskedWord.value)) {
+            Log.i(TAG, "Success")
+            viewModel.endGame(true)
+            displayStats()
+            showFinalScoreDialog(getString(R.string.you_scored, viewModel.score.value, viewModel.games.value))
+        } else if (0 == viewModel.tries.value) {
+            viewModel.endGame(false)
+            showFinalScoreDialog(getString(R.string.you_loose, viewModel.wordToFind.value))
         }
     }
 
@@ -130,6 +142,7 @@ class HangmanFragment : Fragment() {
         Log.d(TAG, "result = "+viewModel.maskedWord.value)
         binding.gameresult.resultWord.text = viewModel.maskedWord.value
         binding.gameresult.triesLeft.text = resources.getQuantityString(R.plurals.tries_left, viewModel.tries.value!!, viewModel.tries.value!!)
+        binding.gameresult.imgPendu.setImageDrawable(ResourcesCompat.getDrawable(resources, imgs[viewModel.tries.value!!], requireContext().theme))
     }
 
     /*
